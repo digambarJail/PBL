4d716786e46b0485e04a28eac1db7da4cddaa0ec
digambarJail <ddjail2004@gmail.com>
2024-02-16 10:16:18 +0530

    login page modularized

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
     <a href="#" class="flex items-center">
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PICT CONNECT</span>
     </a>
@@ -28,16 +28,16 @@ const NavBar = () => {
           <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">LOGIN</a>
+        <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">LOGIN</a>
         </li>
       </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
 
     </>
   )
 }
 
-export default NavBar
+export default NavBar;
